4488aefa432744b5a5973e297e9c00ff23e54b35
Harold Geumtcheng <geumtchengh@gmail.com>
2021-08-20 21:11:49 +0100

    Add files via upload

---
+++ babe.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{ int n;
+  
+    cout << "babe answer 1 for yes and 0 for no\n\n";
+    cout<<"do you enjoy the time you have with me???\n";
+    cin>>n;
+    
+     if(n==1){
+      cout<<"do you love me babe???\n";
+      cin>>n;
+      if(n==1){
+        cout<<"i love you too babe...\n I've had a great time with you since\n we started all this...\n i think it's time to push things further\n so babe...\n would you be my girlfriend ???\n";
+        cin>>n;
+        if (n==1){
+          cout<<"Great ...\n Now you're officially my girl... \n love ya";
+          }
+          else if(n==0){
+            cout<<"okay will wait till you're ready\n";
+            cout<<"but I'm i wasting my time???\n";
+            cin>>n;
+            if(n==1){
+              cout<<"okay so this is over!!!";
+            }
+           else if(n==0){
+              cout<<"okay babe take your time then\n";
+            }   }
+                            
+        }
+       else if(n==0){
+          cout<<"are you serious right now???\n";
+          cin>>n;
+          if(n==1){
+          cout<<"okay I'm gonna have a bad day...\n stay away from me if you don't want to get hurt";
+         }
+         else {
+           cout<<"okay so now answer seriously...\n";
+            cout<<"do you love me babe???\n";
+      cin>>n;
+      if(n==1){
+        cout<<"i love you too babe...\n I've had a great time with you since\n we started all this\n i think it's time to push things further\n so babe...\n would you be my girlfriend ???\n";
+        cin>>n;
+        if (n==1){
+          cout<<"Great ...\n Now you're officially my girl... \n love ya";
+          }
+          else{
+            cout<<"okay will wait till you're ready\n";
+            cout<<"but I'm i wasting my time???\n";
+            cin>>n;
+            if(n==1){
+              cout<<"okay so this is over!!!";
+            }
+            if(n==0){
+              cout<<"okay babe take your time then??";
+            }
+         } }
+      }
+      
+    }  }
+   else if(n==0){
+      cout<<"babe like seriously?...okay\n";
+      cout<<"do you love me babe???\n";
+      cin>>n;
+      if(n==1){
+        cout<<"i love you too babe...\n I've had a great time with you since\n we started all this\n i think it's time to push things further\n so babe...\n would you be my girlfriend ???\n";
+        cin>>n;
+        if (n==1){
+          cout<<"Great ...\n Now you're officially my girl... \n love ya";
+          }
+          else if(n==0){
+            cout<<"okay will wait till you're ready\n";
+            cout<<"but I'm i wasting my time???\n";
+            cin>>n;
+            if(n==1){
+              cout<<"okay so this is over!!!";
+            }
+           else if(n==0){
+              cout<<"okay babe take your time then\n";
+            }   }
+                
+        }
+      else  if(n==0){
+          cout<<"are you serious right now???\n";
+          cin>>n;
+          if(n==1){
+          cout<<"okay I'm gonna have a bad day...\n stay away from me if you don't want to get hurt";
+         }
+         else {
+           cout<<"okay so now answer seriously...\n";
+            cout<<"do you love me babe???\n";
+      cin>>n;
+      if(n==1){
+        cout<<"i love you too babe...\n I've had a great time with you since\n we started all this\n i think it's time to push things further\n so babe...\n would you be my girlfriend ???\n";
+        cin>>n;
+        if (n==1){
+          cout<<"Great  ...\n Now you're officially my girl...\n love ya...";
+          }
+          else{
+            cout<<"okay will wait till you're ready\n";
+            cout<<"but I'm i wasting my time???\n";
+            cin>>n;
+            if(n==1){
+              cout<<"okay so this is over!!!";
+            }
+            if(n==0){
+              cout<<"okay babe take your time then??";
+            }
+         } }
+      }
+      
+    }
+    }
+
+  return 0;
+}
